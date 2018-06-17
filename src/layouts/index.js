ccc124e72e24aa4947a1a16fa1b44cc0f0a36830
Josh Ferrell <josh@joshferrell.me>
2018-06-17 13:35:10 -0700

    chore: fixed linting errors

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,45 +1,50 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
 
-import Header from '../components/header'
 import { Theme, Atoms, Organisms } from 'vc-components';
 
 const Layout = ({ children, data }) => (
-  <Theme>
-    <Helmet
-      title={data.site.siteMetadata.title}
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]}
-    />
-	<Atoms.Container>
-		<Organisms.DesktopMenu
-			renderLeftNav={() => [
-				<Atoms.NavLink to="/products">Products</Atoms.NavLink>,
-				<Atoms.NavLink to="/pricing">Pricing</Atoms.NavLink>,
-				<Atoms.NavLink to="/support">Support</Atoms.NavLink>
-			]}
-			renderRightNav={() => [
-				<Atoms.NavLink to="/about">Sign In</Atoms.NavLink>,
-				<Atoms.ArrowButtonLink to="/sign-up" type="secondary" squared>
-					Try Free
-				</Atoms.ArrowButtonLink>
+	<Theme>
+		<Helmet
+			title={data.site.siteMetadata.title}
+			meta={[
+				{ name: 'description', content: 'Sample' },
+				{ name: 'keywords', content: 'sample, something' }
 			]}
 		/>
-	    <div>
-	      {children()}
-	    </div>
-	</Atoms.Container>
-</Theme>
-)
+		<Atoms.Container>
+			<header>
+				<Organisms.DesktopMenu
+					renderLeftNav={() => [
+						<Atoms.NavLink to="/products">Products</Atoms.NavLink>,
+						<Atoms.NavLink to="/pricing">Pricing</Atoms.NavLink>,
+						<Atoms.NavLink to="/support">Support</Atoms.NavLink>
+					]}
+					renderRightNav={() => [
+						<Atoms.NavLink to="/about">Sign In</Atoms.NavLink>,
+						<Atoms.ArrowButtonLink to="/sign-up" type="secondary" squared>
+						Try Free
+						</Atoms.ArrowButtonLink>
+					]}
+				/>
+			</header>
+			<main>{children()}</main>
+			<footer>
+				testeroo
+			</footer>
+		</Atoms.Container>
+	</Theme>
+);
 
 Layout.propTypes = {
-  children: PropTypes.func,
-}
+	children: PropTypes.func.isRequired,
+	data: PropTypes.shape({
+		site: PropTypes.object
+	}).isRequired
+};
 
-export default Layout
+export default Layout;
 
 export const query = graphql`
   query SiteTitleQuery {
@@ -49,4 +54,4 @@ export const query = graphql`
       }
     }
   }
-`
+`;
