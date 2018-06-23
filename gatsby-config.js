0cc7db6b7db4ce367e9ce78f54f84b81adb51e78
Josh Ferrell <josh@joshferrell.me>
2018-06-22 21:50:59 -0700

    feat: netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,10 @@
 module.exports = {
 	siteMetadata: {
-		title: 'Voice Computer',
+		title: 'Voice Computer'
 	},
 	plugins: [
 		'gatsby-plugin-react-helmet',
-		'gatsby-plugin-styled-components'
+		'gatsby-plugin-styled-components',
+		'gatsby-plugin-netlify-cms'
 	]
 };
