4ea0575af576f139343d881c2e2c0dbe79487920
Josh Ferrell <josh@joshferrell.me>
2018-07-25 21:12:12 -0700

    fix: styled components ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,16 +1,22 @@
-import React from 'react'
-import { Provider } from 'react-redux'
-import { renderToString } from 'react-dom/server'
+import React from 'react';
+import { Provider } from 'react-redux';
+import { renderToString } from 'react-dom/server';
+import { ServerStyleSheet } from 'styled-components';
+import createStore from './src/redux';
 
-import createStore from './src/redux'
-
-export const replaceRenderer = ({ bodyComponent, replaceBodyHTMLString }) => {
+export const replaceRenderer = ({ bodyComponent, replaceBodyHTMLString, setHeadComponents }) => {
     const store = createStore();
+	const sheet = new ServerStyleSheet();
 
     const ConnectedBody = () => (
         <Provider store={store}>
-            {bodyComponent}
+			<StyleSheetManager sheet={sheet.instance}>
+				{bodyComponent}
+			</StyleSheetManager>
         </Provider>
-    )
-    replaceBodyHTMLString(renderToString(<ConnectedBody/>))
+    );
+
+	const styleElement = sheet.getStyleElement();
+    replaceBodyHTMLString(renderToString(<ConnectedBody/>));
+	setHeadComponents(styleElement);
 }
